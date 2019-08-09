b4cbf00f950e873fbb977ffc9feb042fa59cac8c
ilhamsj <ilhamsaputrajati@gmail.com>
2019-08-09 11:00:31 +0700

    proc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,11 +10,7 @@
     <title>{{ env('app_name')}}@yield('title')</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('themes/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('themes/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Custom styles for this page -->
     <link href="{{ asset('themes/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
@@ -239,7 +235,8 @@
     @include('layouts._logout')
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('themes/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- <script src="{{ asset('themes/vendor/jquery/jquery.min.js') }}"></script> --}}
     <script src="{{ asset('themes/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
