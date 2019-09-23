64ee9af26302eb8330ad00e29d78da08ae9ffc9e
ilhamsj <ilhamsaputrajati@gmail.com>
2019-09-23 12:36:32 +0700

    serabutan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="row h-100 justify-content-center align-items-center">
         <div class="col-md-8">
-            <h1>Welcome to <span class="text-danger">Serabutan</span></h1>
+            <h1>Welcome to <span class="text-danger">Serabutan Millenial</span></h1>
             <p class="lead">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum autem ex neque in quidem? Pariatur quisquam incidunt possimus magnam, voluptatum porro debitis adipisci aspernatur sequi sapiente modi alias, nisi quam.
             </p>
